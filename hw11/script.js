14599d8200b5e31f9332b21f55ac6b77f1d4daf7
Masha Titova <masha.titova32@gmail.com>
2023-05-07 19:35:46 +0300

    hw11

--- hw11/script.js
+++ hw11/script.js
@@ -2,6 +2,7 @@ arrLength = 0;
 arrLength = parseInt(prompt('Enter array length'));
 
 arr = [];
+console.log('Emply array', arr);
 
 for(i = 0; i < arrLength; i++){
     elementArr = prompt(`Enter #${i + 1} element of array`);
@@ -13,7 +14,4 @@ console.log('Filled array', arr);
 arr.sort((a,b) => a - b);
 
 console.log('Sorted array', arr);
-
-arr.splice(1,3);
-
-console.log('Removed item from array', arr);
+console.log('Removed item from array', arr.slice(1,4));
